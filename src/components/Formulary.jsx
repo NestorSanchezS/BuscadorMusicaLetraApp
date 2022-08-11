91765bca20bfc49e9f035a6e9fcf80e5f4a1c642
NestorSanchez <nesaenz.69@gmail.com>
2022-08-10 20:25:01 -0500

    add  element artis and music

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 export const Formulary = () => {
+  const [dataForm, setDataForm] = useState({
+    artist: "",
+    music: "",
+  });
   return (
     <form>
       <legend>Busca por Artista y Cancion</legend>
       <div className="form-grid">
         <div>
           <label>Artista</label>
-          <input type="text" name="artist" placeholder="Nombre Artista" />
+          <input
+            type="text"
+            name="artist"
+            placeholder="Nombre Artista"
+            value={dataForm.artist}
+            onChange={({ target }) => setDataForm(target.value)}
+          />
         </div>
+        <div>
+          <label>Cancion</label>
+          <input
+            type="text"
+            name="music"
+            placeholder="Nombre cancion"
+            value={dataForm.music}
+            onChange={({ target }) => setDataForm(target.value)}
+          />
+        </div>
+        <input type="submit" value="Search" />
       </div>
     </form>
   );

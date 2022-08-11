c7483eb2c4d035c1f817fb774ffa5df304eaa235
NestorSanchez <nesaenz.69@gmail.com>
2022-08-10 20:12:06 -0500

    edit and add element component formulary

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const Formulary = () => {
-  return <form>Formulary</form>;
+  return (
+    <form>
+      <legend>Busca por Artista y Cancion</legend>
+      <div className="form-grid">
+        <div>
+          <label>Artista</label>
+          <input type="text" name="artist" placeholder="Nombre Artista" />
+        </div>
+      </div>
+    </form>
+  );
 };
